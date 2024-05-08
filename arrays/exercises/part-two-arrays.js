3d2b22b951a35d039cc423659843d1e8a6b86c57
tatianasnook <tatsnook@gmail.com>
2024-05-07 19:29:34 -0500

    array exercises are completed

--- arrays/exercises/part-two-arrays.js
+++ arrays/exercises/part-two-arrays.js
@@ -1,11 +1,25 @@
 let cargoHold = ['oxygen tanks', 'space suits', 'parrot', 'instruction manual', 'meal packs', 'slinky', 'security blanket'];
 
 //1) Use bracket notation to replace ‘slinky’ with ‘space tether’. Print the array to confirm the change.
+cargoHold.splice(5, 1, 'space tether');
+console.log(cargoHold);
 
 //2) Remove the last item from the array with pop. Print the element removed and the updated array.
+let removedItem = cargoHold.pop();
+console.log(removedItem);
+console.log(cargoHold);
 
 //3) Remove the first item from the array with shift. Print the element removed and the updated array.
+let removedFirstItem = cargoHold.shift();
+console.log(removedFirstItem);
+console.log(cargoHold);
 
-//4) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and ‘20 meters’ to the the array - the number at the start and the string at the end. Print the updated array to confirm the changes.
+//4) Unlike pop and shift, push and unshift require arguments inside the (). Add the items 1138 and 
+// ‘20 meters’ to the the array - the number at the start and the string at the end. 
+// Print the updated array to confirm the changes.
+cargoHold.unshift(1138);
+cargoHold.push('20 meters');
+console.log(cargoHold);
 
 //5) Use a template literal to print the final array and its length.
+console.log(`Finale array: ${cargoHold} and its length: ${cargoHold.length}`)
