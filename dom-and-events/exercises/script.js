5bc34d5f9e9350d9050088cbe3f694bbad1ed749
tatianasnook <tatsnook@gmail.com>
2024-08-01 12:24:47 -0500

    DOM and events exercise is completed

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,8 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
-    
+    button.addEventListener('click', function() {
+        paragraph.innerHTML = 'Houston, we have liftoff!'
+    })
+
+    missionAbort.addEventListener('mouseover', function( event ) {
+        event.target.style.backgroundColor = 'red';
+    })
+
+    missionAbort.addEventListener('mouseout', function( event ) {
+        event.target.style.backgroundColor = ''
+    })
+
+    missionAbort.addEventListener('click', function() {
+        let response = window.confirm('Are you sure you want to abort the mission?');
+        if (response){
+            window.alert('Mission aborted! Space shuttle returning home');
+        }
+    })
 }
 
 window.addEventListener("load", init);
