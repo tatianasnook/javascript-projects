88c66e51bb21eb803714d41933a13718b86f4dcf
tatianasnook <tatsnook@gmail.com>
2024-08-15 20:06:16 -0500

    studio is completed

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,32 @@
-//TODO: Add Your Code Below
+
+window.addEventListener('load', function(){
+  this.fetch('https://handlers.education.launchcode.org/static/astronauts.json')
+  .then(function(response){
+    response.json().then(function(data){
+      console.log(data);
+
+      data.sort(function(a,b){
+        return b.hoursInSpace - a.hoursInSpace;
+      });
+      
+      const container = document.getElementById('container');
+      container.innerHTML = `<h2>Total Number of Astronauts: ${data.length}</h2>`;
+
+      for(let i = 0; i < data.length; i++){
+        container.innerHTML += `
+          <div class="astronaut">
+            <div class="bio">
+              <h3>${data[i].firstName} ${data[i].lastName}</h3>
+              <ul>
+                <li>Hours in space: ${data[i].hoursInSpace}</li>
+                <li style="color: ${data[i].active ? 'green' : 'black'}">Active: ${data[i].active}</li>
+                <li>Skills: ${data[i].skills}</li>
+              </ul>
+            </div>
+            <img class="avatar" src="${data[i].picture}">
+          </div>
+        `;
+      }
+    })
+  })
+})
